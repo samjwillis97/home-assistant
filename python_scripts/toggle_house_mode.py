bc90c09999cf20547f88fe6531cdddb51a5824aa
samjwillis97 <sam@williscloud.org>
2026-01-11 20:53:06 +1100

    Using python script

--- python_scripts/toggle_house_mode.py
+++ python_scripts/toggle_house_mode.py
@@ -1,7 +1,6 @@
 NOTHING_MODE_NAME = "nothing"
 
 VALID_MODE_NAMES = [
-    NOTHING_MODE_NAME,
     "away",
     "sleep",
     "bedtime"
@@ -10,10 +9,16 @@ VALID_MODE_NAMES = [
 # `data` is available as builtin and is a dictionary with the input data.
 mode = data.get("mode", NOTHING_MODE_NAME)
 
-if mode not in VALID_MODE_NAMES:
+# `logger` and `time` are available as builtin without the need of explicit import.
+if mode == NOTHING_MODE_NAME:
+    logger.info("No house mode change requested.")
+elif mode not in VALID_MODE_NAMES:
     logger.error("Invalid mode name: '%s'.", mode)
 else:
-# `logger` and `time` are available as builtin without the need of explicit import.
     logger.info("Toggling house mode to '%s'.", mode)
-
+    for name in VALID_MODE_NAMES
+        if mode == name:
+            hass.services.call("input_boolean", "turn_on", { "entity_id": f"input_boolean.house_mode_{name}" }, False)
+        else:
+            hass.services.call("input_boolean", "turn_off", { "entity_id": f"input_boolean.house_mode_{name}" }, False)
 
