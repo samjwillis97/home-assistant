be5ac0a79e9b9439e94c77da93862ad8c612e570
samjwillis97 <sam@williscloud.org>
2026-01-11 18:26:02 +1100

    Adding first python script

---
+++ python_scripts/toggle_house_mode.py
@@ -0,0 +1,20 @@
+NOTHING_MODE_NAME = "nothing"
+
+VALID_MODE_NAMES = [
+    NOTHING_MODE_NAME,
+    "away",
+    "sleep",
+    "bedtime"
+]
+
+# `data` is available as builtin and is a dictionary with the input data.
+mode = data.get("mode", NOTHING_MODE_NAME)
+
+if mode not in VALID_MODE_NAMES:
+    logger.error("Invalid mode name: '%s'.", mode)
+    return
+
+# `logger` and `time` are available as builtin without the need of explicit import.
+logger.info("Toggling house mode to '%s'.", mode)
+
+
